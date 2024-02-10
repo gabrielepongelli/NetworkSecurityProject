b47f169be4d63b89ef302eddbfcf89b580c53d2b
Gabriele Pongelli <pongelligabriele@gmail.com>
2024-02-10 12:00:26 +0000

    Removed unnecessary commands from the setup script.

--- setup.sh
+++ setup.sh
@@ -6,25 +6,12 @@ usage() {
 }
 
 start() {
-    #sudo nmcli n off
-    #sudo nmcli r wifi off
     sudo nmcli device set $1 managed no
-    #sudo systemctl stop NetworkManager.service
-    sudo systemctl stop dnsmasq.service
-    #sudo rfkill unblock wlan
-    #sudo ifconfig $1 192.168.123.1/24 up
-    sudo ip addr add 192.168.123.1/24 dev $1
     sudo ip link set $1 up
 }
 
 stop() {
-    #sudo ifconfig $1 down
     sudo ip link set $1 down
-    sudo ip addr del 192.168.123.1/24 dev $1
-    sudo systemctl start dnsmasq.service
-    #sudo systemctl start NetworkManager.service
-    #sudo nmcli n on
-    #sudo nmcli r wifi on
     sudo nmcli device set $1 managed yes
 }
 
